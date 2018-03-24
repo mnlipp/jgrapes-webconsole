d3225977270aaf79e9487858cfd2ab794d3fea32
Michael N. Lipp <mnl@mnl.de>
2018-03-24 19:26:34 +0100

    Fixed name.

--- HttpPortalDemo/src/org/jgrapes/portal/demo/NewPortalSessionPolicy.java
+++ HttpPortalDemo/src/org/jgrapes/portal/demo/NewPortalSessionPolicy.java
@@ -75,9 +75,9 @@ public class NewPortalSessionPolicy extends Component {
 		fire(new AddPortletRequest(event.event().event().renderSupport(), 
 				MarkdownDisplayPortlet.class.getName(),
 				Portlet.RenderMode.Preview)
-				.addPreference(Preferences.TITLE, "Demo Portal")
-				.addPreference(Preferences.PREVIEW_SOURCE, "A Demo Portal")
-				.addPreference(Preferences.EDITABLE_BY,  Collections.EMPTY_SET),
+				.addProperty(Preferences.TITLE, "Demo Portal")
+				.addProperty(Preferences.PREVIEW_SOURCE, "A Demo Portal")
+				.addProperty(Preferences.EDITABLE_BY,  Collections.EMPTY_SET),
 				portalSession);
 	}
 
