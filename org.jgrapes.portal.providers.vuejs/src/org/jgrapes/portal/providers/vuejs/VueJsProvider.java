ce21928aa90c6f986f8acd51c27ada118aac04b5
Michael N. Lipp <mnl@mnl.de>
2018-08-06 18:18:00 +0200

    Added vue.js.

---
+++ org.jgrapes.portal.providers.vuejs/src/org/jgrapes/portal/providers/vuejs/VueJsProvider.java
@@ -0,0 +1,76 @@
+/*
+ * JGrapes Event Driven Framework
+ * Copyright (C) 2017-2018 Michael N. Lipp
+ * 
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jgrapes.portal.providers.vuejs;
+
+import freemarker.core.ParseException;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.TemplateNotFoundException;
+
+import java.io.IOException;
+
+import org.jgrapes.core.Channel;
+import org.jgrapes.core.Event;
+import org.jgrapes.core.Manager;
+import org.jgrapes.core.annotation.Handler;
+import org.jgrapes.portal.base.PageResourceProvider;
+import org.jgrapes.portal.base.PortalSession;
+import org.jgrapes.portal.base.events.AddPageResources;
+import org.jgrapes.portal.base.events.AddPageResources.ScriptResource;
+import org.jgrapes.portal.base.events.PortalReady;
+
+/**
+ * Provider for the [Vue.js](http://www.vuejs.org/) library.
+ */
+public class VueJsProvider extends PageResourceProvider {
+
+    /**
+     * Creates a new component with its channel set to the given 
+     * channel.
+     * 
+     * @param componentChannel the channel that the component's 
+     * handlers listen on by default and that 
+     * {@link Manager#fire(Event, Channel...)} sends the event to 
+     */
+    public VueJsProvider(Channel componentChannel) {
+        super(componentChannel);
+    }
+
+    /**
+     * On {@link PortalReady}, fire the appropriate {@link AddPageResources}.
+     *
+     * @param event the event
+     * @param portalSession the portal session
+     * @throws TemplateNotFoundException the template not found exception
+     * @throws MalformedTemplateNameException the malformed template name exception
+     * @throws ParseException the parse exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Handler(priority = 100)
+    public void onPortalReady(PortalReady event, PortalSession portalSession)
+            throws TemplateNotFoundException, MalformedTemplateNameException,
+            ParseException, IOException {
+        String minExt = event.renderSupport()
+            .useMinifiedResources() ? ".min" : "";
+        portalSession.respond(new AddPageResources()
+            .addScriptResource(new ScriptResource()
+                .setProvides(new String[] { "vuejs.org" })
+                .setScriptUri(event.renderSupport().pageResource(
+                    "vue-2.5.17/vue" + minExt + ".js"))));
+    }
+}
