3c2231fc38591799107d3e250e93b8f8fc2d173d
Michael N. Lipp <mnl@mnl.de>
2019-12-07 23:00:24 +0100

    Start using modules.

--- org.jgrapes.portal.jqueryui/resources/org/jgrapes/portal/jqueryui/jquiportal.js
+++ org.jgrapes.portal.jqueryui/resources/org/jgrapes/portal/jqueryui/jquiportal.js
@@ -1,6 +1,6 @@
 /*
  * JGrapes Event Driven Framework
- * Copyright (C) 2016, 2018  Michael N. Lipp
+ * Copyright (C) 2016, 2019  Michael N. Lipp
  *
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU Affero General Public License as published by 
