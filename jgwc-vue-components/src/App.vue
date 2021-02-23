6937414b0b787a11ea0313d95971f09dc04c34da
Michael N. Lipp <mnl@mnl.de>
2021-02-23 23:06:59 +0100

    Testing...

---
+++ jgwc-vue-components/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TestButton text="Hello!"></TestButton>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+// import HelloWorld from "./components/HelloWorld.vue";
+import TestButton from "./components/TestButton.vue";
+
+@Component({
+  components: {
+    TestButton
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
