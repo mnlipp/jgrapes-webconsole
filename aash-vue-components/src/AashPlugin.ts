77d1b103da875a3ddbd5e0454c0dbc9920e8e384
Michael N. Lipp <mnl@mnl.de>
2022-03-22 18:41:57 +0100

    Fix doc.

--- aash-vue-components/src/AashPlugin.ts
+++ aash-vue-components/src/AashPlugin.ts
@@ -1,12 +1,16 @@
 /**
  * Provides the components from the library and a global context 
- * `$aash` of type {@link Aash}
+ * `$aash` that implements the interface `Aash`. 
  *
  * @module AashPlugin
  */
 
 import { App, Ref, ref } from 'vue'
 import { provideApi, getApi } from "./AashUtil";
+import AashAccordionComponent from "./components/accordion/AashAccordion.vue";
+import * as AashAccordion from "./components/accordion/AashAccordion";
+import AashAccordionSectionComponent from "./components/accordion/AashAccordionSection.vue";
+import * as AashAccordionSection from "./components/accordion/AashAccordionSection";
 import AashDisclosureButtonComponent from "./components/disclosure/AashDisclosureButton.vue";
 import * as AashDisclosureButton from "./components/disclosure/AashDisclosureButton";
 import AashDropdownMenuComponent from "./components/dropdown-menu/AashDropdownMenu.vue";
@@ -27,12 +31,21 @@ export { provideApi, getApi,
 };
 
 let disclosureRegistry = new Map();
+let idCounter = 0;
+    
 
 /**
  * The context provided by the Aash library as global context `$aash`.
  * @memberOf module:AashPlugin
  */
 interface Aash {
+    /**
+     * Generates a unique id.
+     * @function
+     * @returns the id
+     */
+    generateId(): string;
+    
     /**
      * Returns the `Vuew.Ref` the represents the state of the disclosure
      * with the given `id`.
@@ -58,6 +71,11 @@ interface Aash {
 }
 
 let aash: Aash = {
+    
+    generateId() {
+        return "aash-id-" + ++idCounter;
+    },
+    
     disclosureData(id) {
         if (disclosureRegistry.has(id)) {
             return disclosureRegistry.get(id);
@@ -84,5 +102,7 @@ export default {
         app.component('aash-modal-dialog', AashModalDialogComponent);
         app.component('aash-tablist', AashTablistComponent);
         app.component('aash-tree-view', AashTreeViewComponent);
+        app.component('aash-accordion', AashAccordionComponent);
+        app.component('aash-accordion-section', AashAccordionSectionComponent);
   }
 }
