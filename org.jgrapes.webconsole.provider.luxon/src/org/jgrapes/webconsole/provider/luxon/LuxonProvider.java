136ef46c89e5ac157c689ba6c399b276c85f3a83
Michael N. Lipp <mnl@mnl.de>
2022-04-19 19:22:40 +0200

    First version.

---
+++ org.jgrapes.webconsole.provider.luxon/src/org/jgrapes/webconsole/provider/luxon/LuxonProvider.java
@@ -0,0 +1,73 @@
+/*
+ * JGrapes Event Driven Framework
+ * Copyright (C) 2022 Michael N. Lipp
+ * 
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Affero General Public License as published by 
+ * the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along 
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jgrapes.webconsole.provider.luxon;
+
+import freemarker.core.ParseException;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.TemplateNotFoundException;
+import java.io.IOException;
+import org.jgrapes.core.Channel;
+import org.jgrapes.core.Event;
+import org.jgrapes.core.Manager;
+import org.jgrapes.core.annotation.Handler;
+import org.jgrapes.webconsole.base.ConsoleSession;
+import org.jgrapes.webconsole.base.PageResourceProvider;
+import org.jgrapes.webconsole.base.events.AddPageResources;
+import org.jgrapes.webconsole.base.events.AddPageResources.ScriptResource;
+import org.jgrapes.webconsole.base.events.ConsoleReady;
+
+/**
+ * Provider for the [luxon](https://moment.github.io/luxon/) library.
+ */
+public class LuxonProvider extends PageResourceProvider {
+
+    /**
+     * Creates a new component with its channel set to the given 
+     * channel.
+     * 
+     * @param componentChannel the channel that the component's 
+     * handlers listen on by default and that 
+     * {@link Manager#fire(Event, Channel...)} sends the event to 
+     */
+    public LuxonProvider(Channel componentChannel) {
+        super(componentChannel);
+    }
+
+    /**
+     * On {@link ConsoleReady}, fire the appropriate {@link AddPageResources}.
+     *
+     * @param event the event
+     * @param consoleSession the web console session
+     * @throws TemplateNotFoundException the template not found exception
+     * @throws MalformedTemplateNameException the malformed template name exception
+     * @throws ParseException the parse exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Handler(priority = 100)
+    public void onConsoleReady(ConsoleReady event,
+            ConsoleSession consoleSession)
+            throws TemplateNotFoundException, MalformedTemplateNameException,
+            ParseException, IOException {
+        consoleSession.respond(new AddPageResources()
+            .addScriptResource(new ScriptResource()
+                .setScriptType("module").setProvides("luxon")
+                .setScriptUri(event.renderSupport().pageResource(
+                    "luxon/build/es6/luxon.js"))));
+    }
+}
