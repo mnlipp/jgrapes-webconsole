fcd829bed1ff91bdce866877ab07d703e4227c4f
Michael N. Lipp <mnl@mnl.de>
2024-06-29 22:53:15 +0200

    Update build.

--- org.jgrapes.webconlet.messagebox/rollup.config.mjs
+++ org.jgrapes.webconlet.messagebox/rollup.config.mjs
@@ -1,18 +1,21 @@
 import typescript from 'rollup-plugin-typescript2';
-import postcss from 'rollup-plugin-postcss'
+import postcss from 'rollup-plugin-postcss';
 
-let module = "build/generated/resources/org/jgrapes/webconlet/messagebox/MessageBox-functions.js"
+let packagePath = "org/jgrapes/webconlet/messagebox";
+let baseName = "MessageBox"
+let module = "build/generated/resources/" + packagePath 
+    +  "/" + baseName + "-functions.js";
 
 let pathsMap = {
     "vue": "../../page-resource/vue/vue.esm-browser.js",
     "jgconsole": "../../console-base-resource/jgconsole.js",
     "jgwc": "../../page-resource/jgwc-vue-components/jgwc-components.js",
-    "l10nBundles": "./MessageBox-l10nBundles.ftl.js"
+    "l10nBundles": "./" + baseName + "-l10nBundles.ftl.js"
 }
 
 export default {
   external: ['vue', 'aash-plugin', 'jgconsole', 'jgwc', 'l10nBundles'],
-  input: "src/org/jgrapes/webconlet/messagebox/browser/MessageBox-functions.ts",
+  input: "src/" + packagePath + "/browser/" + baseName + "-functions.ts",
   output: [
     {
       format: "esm",
